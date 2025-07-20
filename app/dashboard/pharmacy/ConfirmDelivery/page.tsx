f77ad162a8e8857ec7f58731f7cd7952c4f4fd40
Abirpaul007 <abirpaulbkp@gmail.com>
2025-07-20 20:45:35 +0530

    update

--- app/dashboard/pharmacy/ConfirmDelivery/page.tsx
+++ app/dashboard/pharmacy/ConfirmDelivery/page.tsx
@@ -15,7 +15,7 @@ interface Order {
   totalValue: number;
 }
 
-const CONTRACT_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"; // Replace with actual deployed address
+const CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // Replace with actual deployed address
 
 export default function TrackOrdersPage() {
   const [orders, setOrders] = useState<Order[]>([]);
